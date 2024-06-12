f0efde3eb236fae819c72e7982201abb0815903c
mike2ox <firefinger07@gmail.com>
2024-06-12 20:13:29 +0900

    feat: open example file by using fs::read_to_string

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    // dbg!(&args);
+
+    let target = &args[1]; // 찾고자 하는 문자열
+    let file_path = &args[2]; // 파일 경로
+
+    // println!("Searching for {}", query);
+    // println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
