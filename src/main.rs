03db5b71efbc910f84ba089ba13cf8ed8bfb9034
mike2ox <firefinger07@gmail.com>
2024-06-26 10:03:27 +0900

    feat: Apply error handling using Result type

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     // dbg!(&args);
 
-    let config = Config::new(&args);
+    // unwrap_or_else는 panic!이 아닌 애러를 처리할 수 있도록 해줌
+    // Result가 OK를 반환하면 unwrap가 동일한 동작을 함.
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        // 여기는 클로저로서 err를 받아서 처리하는 부분
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     // println!("Searching for {}", target);
     // println!("In file {}", file_path);
 
@@ -22,10 +29,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // Result를 사용해서 성공시 Config, 실패시 &'static str(lifetime은 'static)를 갖는 Result를 반환
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
         let target = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { target, file_path }
+        Ok(Config { target, file_path })
     }
 }
