d90cd7f620002704b40a501d47e797aeda737dae
mike2ox <firefinger07@gmail.com>
2024-07-03 20:01:38 +0900

    feat: create search_case_insensitive for finding same sentence

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,32 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
+
+// 대소문자 구분하는 검색 함수
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let _query = query.to_lowercase(); // 작업 추가 가능
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&_query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
 
 // 파일을 읽어서 출력하는 함수(SoC)
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // let contents =
     //     fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     let contents = fs::read_to_string(config.file_path)?;
-    for line in search(&config.target, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.target, &contents)
+    } else {
+        search(&config.target, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
     Ok(())
@@ -15,8 +35,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 // 2. ? 연산자를 사용해서 expect호출을 제거(panic!대신 호출하는 쪽에서 처리할 수 있도록 함수에서 직접 에러값을 반환)
 // 3. ()를 사용한다는건 run을 호출하여 side effect에 대해서만 처리하겠다는 표현
 pub struct Config {
-    target: String,
-    file_path: String,
+    pub target: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -29,13 +50,46 @@ impl Config {
         let target = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { target, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            target,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 
     #[test]
     fn one_result() {
